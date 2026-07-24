b2666b93c774ca205bf19ac60aa8bdf25f7a100a
Mjolnir2307 <eez227536@ee.iitd.ac.in>
2026-07-25 01:13:02 +0530

    subject level analysis added in extractor.py

--- quants/_data/ntu_rgb/utils/extractor.py
+++ quants/_data/ntu_rgb/utils/extractor.py
@@ -1,30 +1,19 @@
 import os
 import pickle
+import argparse
+import matplotlib.pyplot as plt
 from tools import *
 
-# Missing file removal
-dataDir = './data/'
-reqFiles= [] # The required filed
-dataFiles = os.listdir(dataDir)
-dataFiles_filtered = [] # The filtered files with full data
-missingFiles = list(load_missing_file('./utils/missingSkeletons.txt').keys())
+parser = argparse.ArgumentParser()
+parser.add_argument('--mode',
+                    type=str,
+                    default='analyzer')
+parser.add_argument('--analyzeMode',
+                    type=str,
+                    default='plotSubjects')
+args = parser.parse_args()
 
-cntrPos = 0
-print('Total length: '+str(len(dataFiles)))
 
-for fileCurr in dataFiles:
-    if(fileCurr[:-9] in missingFiles):
-        pass
-    else:
-        cntrPos = cntrPos + 1
-        dataFiles_filtered.append(fileCurr)
-
-dataFiles = dataFiles_filtered
-
-print('Total positives: '+str(cntrPos))
-print('Total length: '+str(len(dataFiles)))
-
-# Action-class based removal
 reqClass = ['A099',
             'A027',
             'A007',
@@ -36,29 +25,85 @@ reqClass = ['A099',
             'A033',
             'A049']
 
-cntrPos = 0
-cntrNeg = 0
 
-for item in os.listdir(dataDir):
-    for reqItem in reqClass:
-        if(reqItem in item):
-            cntrPos = cntrPos + 1
-            reqFiles.append(item)
-            break
-        else:
+
+if(args.mode == 'filter'):
+
+    # Missing file removal
+    dataDir = './data/'
+    reqFiles= [] # The required filed
+    dataFiles = os.listdir(dataDir)
+    dataFiles_filtered = [] # The filtered files with full data
+    missingFiles = list(load_missing_file('./utils/missingSkeletons.txt').keys())
+
+    cntrPos = 0
+    print('Total length: '+str(len(dataFiles)))
+
+    for fileCurr in dataFiles:
+        if(fileCurr[:-9] in missingFiles):
             pass
+        else:
+            cntrPos = cntrPos + 1
+            dataFiles_filtered.append(fileCurr)
 
-cntrNeg = len(dataFiles) - cntrPos
+    dataFiles = dataFiles_filtered
 
-print('Total positives: '+str(cntrPos))
-print('Total Negatives: '+str(cntrNeg))
+    print('Total positives: '+str(cntrPos))
+    print('Total length: '+str(len(dataFiles)))
 
-with open('./utils/reqFiles',"wb") as file:
-    pickle.dump(reqFiles,file)    
+    # Action-class based removal
+    
+    cntrPos = 0
+    cntrNeg = 0
 
-reqFiles_txt = open('./utils/reqFiles.txt',"w")
-for idx, item in enumerate(reqFiles):
-    if(idx != (len(reqFiles)-1)):
-        reqFiles_txt.write(str(item)+'\n')
-    else:
-        reqFiles_txt.write(str(item))
+    for item in os.listdir(dataDir):
+        for reqItem in reqClass:
+            if(reqItem in item):
+                cntrPos = cntrPos + 1
+                reqFiles.append(item)
+                break
+            else:
+                pass
+
+    cntrNeg = len(dataFiles) - cntrPos
+
+    print('Total positives: '+str(cntrPos))
+    print('Total Negatives: '+str(cntrNeg))
+
+    with open('./utils/reqFiles',"wb") as file:
+        pickle.dump(reqFiles,file)    
+
+    reqFiles_txt = open('./utils/reqFiles.txt',"w")
+    for idx, item in enumerate(reqFiles):
+        if(idx != (len(reqFiles)-1)):
+            reqFiles_txt.write(str(item)+'\n')
+        else:
+            reqFiles_txt.write(str(item))
+
+if(args.mode == 'analyzer'):
+
+    with open('./utils/reqFiles',"rb")  as file:
+        reqFiles = pickle.load(file)
+
+    if(args.analyzeMode == 'countSubjects'):
+        subjectList = []
+        for file in reqFiles:
+            if(file[8:12] not in subjectList):
+                subjectList.append(file[8:12])
+        print(np.sort(subjectList))
+
+    if(args.analyzeMode == 'plotSubjects'):
+        dataMat = np.zeros(shape=(106,1))
+        for file in reqFiles:
+            currIdx = int(file[9:12])-1
+            dataMat[currIdx,0] = dataMat[currIdx,0] + 1
+
+        print(min(dataMat))
+        print(max(dataMat))
+
+        plt.bar(np.arange(1,107),dataMat[:,0])
+        plt.show()
+
+
+
+    
