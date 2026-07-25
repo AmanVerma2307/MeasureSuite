92c34f555e0498d69de32abf32c4df3d3707e3ec
Mjolnir2307 <eez227536@ee.iitd.ac.in>
2026-07-25 12:08:30 +0530

    exploratory data analysis done

--- quants/_data/ntu_rgb/utils/extractor.py
+++ quants/_data/ntu_rgb/utils/extractor.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import argparse
+import seaborn as sns
 import matplotlib.pyplot as plt
 from tools import *
 
@@ -13,7 +14,6 @@ parser.add_argument('--analyzeMode',
                     default='plotSubjects')
 args = parser.parse_args()
 
-
 reqClass = ['A099',
             'A027',
             'A007',
@@ -25,7 +25,16 @@ reqClass = ['A099',
             'A033',
             'A049']
 
-
+classMapping = {'A099':0,
+                'A027':1,
+                'A007':2,
+                'A098':3,
+                'A102':4,
+                'A035':5,
+                'A069':6,
+                'A028':7,
+                'A033':8,
+                'A049':9}
 
 if(args.mode == 'filter'):
 
@@ -52,7 +61,6 @@ if(args.mode == 'filter'):
     print('Total length: '+str(len(dataFiles)))
 
     # Action-class based removal
-    
     cntrPos = 0
     cntrNeg = 0
 
@@ -103,6 +111,56 @@ if(args.mode == 'analyzer'):
 
         plt.bar(np.arange(1,107),dataMat[:,0])
         plt.show()
+
+    if(args.analyzeMode == 'countActions'):
+        dataMat = np.zeros(shape=(10,1))
+        for file in reqFiles:
+            currIdx = classMapping[(file[16:20])]
+            dataMat[currIdx,0] = dataMat[currIdx,0] + 1
+
+        print(min(dataMat))
+        print(max(dataMat))
+
+        plt.bar(np.arange(1,11),dataMat[:,0])
+        plt.show()
+
+    if(args.analyzeMode == 'countSubjectsActions'):
+
+        subjectList = []
+        for file in reqFiles:
+            if(file[8:12] not in subjectList):
+                subjectList.append(file[8:12])
+        subjectList = list(np.sort(subjectList))
+
+        def plotGramMatrix(cm,
+                           cmap=plt.cm.Blues):
+            """
+            This function prints and plots the confusion matrix.
+            Normalization can be applied by setting `normalize=True`.
+            """
+            ax = sns.heatmap(cm, cmap=cmap, linewidth=0.5, linecolor='black')
+            plt.show()
+
+        dataMat = np.zeros(shape=(106,10))
+        for file in reqFiles:
+            currSubject = int(file[9:12])-1
+            currAction = classMapping[(file[16:20])]
+            dataMat[currSubject, currAction] = dataMat[currSubject, currAction] + 1
+  
+        print(dataMat)
+        plotGramMatrix(dataMat >= 6)
+
+
+
+
+
+
+
+
+
+
+
+        
 
 
 
