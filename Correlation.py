008a3d85e4706845a5fa2e9b63946c6f58a4d58c
Mjolnir2307 <eez227536@ee.iitd.ac.in>
2026-07-29 20:33:23 +0530

    added modularity of correlation plots in correlation.py

--- Correlation.py
+++ Correlation.py
@@ -14,10 +14,35 @@ parser.add_argument('--mode',
                     type=str,
                     default='corrPlots',
                     help="Mode of correlation analysis")
+parser.add_argument('--corrPlot1',
+                    type=str,
+                    default='r',
+                    help="The first measure for correlation plots")
+parser.add_argument('--corrPlot2',
+                    type=str,
+                    default='Ar_star',
+                    help="The second measure for correlation plots")
+parser.add_argument('--corrPlot_pallete',
+                    type=str,
+                    default='rocket',
+                    help="The color pallete to be used for correlation plots")
+
 args = parser.parse_args()
 
-
 embeddingList, datasetList = retList(args.dataset)
+labels = {'r':'$\\mathcal{r}$',
+           'relevance':'R',
+            'psi':'$\\psi$',
+            'Cd':'$C_{d}$',
+            'Ar_star':'$nAr^{*}(\Delta)$',
+            'euclid':'Euclidean dist.',
+            'Kendall':'$\\tau$',
+            'DCG':'nDCG',
+            'err':'err',
+            'U':'U measure',
+            'gre':'GRE',
+            'infAp':'infAp',
+            'rpp':'RPP'}
 
 
 if(args.mode == 'corrPlots'):
@@ -26,22 +51,12 @@ if(args.mode == 'corrPlots'):
                             'full')
     df = make_df(np.array(measureVal))
 
-    labels = ['$\\mathcal{r}$',
-            'R',
-            '$\\psi$',
-            '$C_{d}$',
-            '$Ar(\Delta)$',
-            '$Ar(\Delta)*\\bar{O}$',
-            '$Ar(\Delta)*\\bar{\psi}$',
-            '$\\bar{\psi}*\\bar{O}$',
-            '$nAr^{*}(\Delta)$']
-
-    rocket = sns.color_palette("rocket")
-    sns.jointplot(x="rpp",
-                  y="Ar_star",
+    rocket = sns.color_palette(args.corrPlot_pallete)
+    sns.jointplot(x=args.corrPlot1,
+                  y=args.corrPlot2,
                   data=df,
                   kind="reg",
                   color=rocket[5])
-    plt.ylabel('$nA_r^{*}(\Delta)$',fontsize=14)
-    plt.xlabel('RPP',fontsize=14)
+    plt.xlabel(labels[args.corrPlot1],fontsize=14)
+    plt.ylabel(labels[args.corrPlot2],fontsize=14)
     plt.show()
